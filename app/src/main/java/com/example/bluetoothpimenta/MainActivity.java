fb2cbcb8db13cb9ac220965d2af0ca8d1eb02c9f
Luis Felipe <luisfelipearaujopimenta@gmail.com>
2023-05-18 12:32:51 -0300

    initial commit

--- app/src/main/java/com/example/bluetoothpimenta/MainActivity.java
+++ app/src/main/java/com/example/bluetoothpimenta/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.bluetoothpimenta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView txtBluetooth;
+    private Button btnBluetooth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+        txtBluetooth = (TextView) findViewById(R.id.txtBluetooth);
+        btnBluetooth = (Button) findViewById(R.id.btnBluetooth);
+
+        btnBluetooth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtBluetooth.setText("Mudou");
+
+            }
+        });
+
+}
 }
